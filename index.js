e252de9305d2e428746470aa4a711c96f9fefa24
Regis Mazur <regis_mazur@hotmail.com>
2018-09-23 08:18:38 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+
+const PORT = 4000;
+
+const app = express();
+
+const books = [
+    {
+      title: 'Harry Potter and the Chamber of Secrets',
+      author: 'J.K. Rowling',
+    },
+    {
+      title: 'Jurassic Park',
+      author: 'Michael Crichton',
+    },
+  ];
+
+const typeDefs = gql`
+# Comments in GraphQL are defined with the hash (#) symbol.
+
+# This "Book" type can be used in other type declarations.
+type Book {
+  title: String
+  author: String
+}
+
+# The "Query" type is the root of all GraphQL queries.
+# (A "Mutation" type will be covered later on.)
+type Query {
+  books: [Book]
+}
+`;
+
+const resolvers = {
+    Query: {
+        books: () => books,
+      },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({ app });
+
+app.listen({ port: PORT }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+);
